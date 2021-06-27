2d09734977ac916c1c495b7587f2a204fab096cf
hoandm <hoandm91@gmail.com>
2021-06-28 04:00:21 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoInputComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Filter, FilterButton } from 'src/app/models/filtering.model';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  filterButtons: FilterButton[] = [
+    {type: Filter.All, label: 'All', isActive: true},
+    {type: Filter.Active, label: 'Active', isActive: false},
+    {type: Filter.Completed, label: 'Completed', isActive: false},
+  ]
+
+  length: number = 0
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
